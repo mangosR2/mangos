577273eec39a328f7897f134f57479c308423885
ConanHUN <conan_513@hotmail.com>
2012-10-22 06:38:17 +0200

    Change path and VC

--- project_setup.bat
+++ project_setup.bat
@@ -3,9 +3,9 @@ REM ****************************************************************************
 REM Set your compiler and another features here. Possible VC1, VC11 
 REM currently possible VC10 (MS Visual studio 10 Pro) and VC11 (MS Visual studio 11 or 2012 Pro/Ultimate)
 REM Warning! VS Express edition not supported! Also need check path (below) to VC10/11 binary
-SET compiler=VC11
+SET compiler=VC10
 REM Install path for MaNGOS (in this be created ./bin and ./etc folders)
-SET INSTALL_PATH="C:\\GAMES\\MaNGOS"
+SET INSTALL_PATH="C:\\SingleCore"
 rem Platform for build. Possible Win32, Win64. Warning! Win64 build possible only on 64-bit main OS!
 SET BUILD_PLATFORM=Win32
 rem Count of cores on PC, where project compiled. for speedup only
