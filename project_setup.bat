835de82a890c088bb7809624c759e893fdd31bdb
ConanHUN <conan_513@hotmail.com>
2013-07-01 00:20:44 +0200

    default bat changed

--- project_setup.bat
+++ project_setup.bat
@@ -10,10 +10,10 @@ REM Install path for MaNGOS (in this be created ./bin and ./etc folders)
 SET INSTALL_PATH="C:\\SingleCore"
 
 rem Platform for build. Possible Win32, Win64. Warning! Win64 build possible only on 64-bit main OS!
-SET BUILD_PLATFORM=Win32
+SET BUILD_PLATFORM=Win64
 
 rem Count of cores on PC, where project compiled. for speedup only
-SET CORE_NUMBER=1
+SET CORE_NUMBER=8
 
 rem Used mangos memory manager. Possible managers - STD, TBB (not recommended), FASTMM (default)
 SET MEMORY_MANAGER=FASTMM
