3ecbbda282491b5b8c6872b7df2ad5fe5d44b457
rsa <kris13@bk.ru>
2013-10-21 11:03:12 +0400

    try use atomic lock in LockedVector v2. must give some sppedup, but also may have another results :) testing.

--- src/shared/LockedVector.h
+++ src/shared/LockedVector.h
@@ -33,10 +33,7 @@ namespace ACE_Based
     class LockedVector
     {
         public:
-
-        typedef   MANGOSR2_MUTEX_MODEL         LockType;
-        typedef   ACE_Read_Guard<LockType>     ReadGuard;
-        typedef   ACE_Write_Guard<LockType>    WriteGuard;
+        MANGOSR2_ATOMIC_LOCK_TYPE RefLock;
 
         typedef typename std::vector<T, Allocator>::iterator               iterator;
         typedef typename std::vector<T, Allocator>::const_iterator         const_iterator;
@@ -49,70 +46,80 @@ namespace ACE_Based
 
         public:
             //Constructors
-            explicit LockedVector(const Allocator& alloc = Allocator()) : m_storage(alloc)
+            explicit LockedVector(const Allocator& alloc = Allocator()) : m_storage(alloc), RefLock(0)
             {}
 
             explicit LockedVector(size_type n, const T& value = T(), const Allocator& alloc = Allocator())
-                : m_storage(n,value,alloc)
+                : m_storage(n,value,alloc), RefLock(0)
             {}
 
             virtual ~LockedVector(void)
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_WAIT(RefLock);
             }
 
             void reserve(size_type idx)
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 m_storage.reserve(idx);
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             // Methods
             void push_back(const T& item)
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 m_storage.push_back(item);
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             void insert(iterator pos, size_type n, const T& u ) 
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 m_storage.insert( pos, n, u ); 
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             template <class InputIterator> 
             void insert(iterator pos, InputIterator begin, InputIterator end)
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 m_storage.insert(pos, begin, end);
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             void pop_back()
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 m_storage.pop_back();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             void erase(size_type pos)
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 m_storage.erase(m_storage.begin() + pos);
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             iterator erase(iterator itr)
             {
-                WriteGuard Guard(GetLock());
-                return m_storage.erase(itr);
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                iterator ret = m_storage.erase(itr);
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return ret;
             }
 
             void remove(const T& item)
             {
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 erase(item);
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             void erase(const T& item)
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 for (size_type i = 0; i < m_storage.size();)
                 {
                     if (item == m_storage[i])
@@ -120,225 +127,269 @@ namespace ACE_Based
                     else
                         ++i;
                 }
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             T* find(const T& item)
             {
-                ReadGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                T* ret = NULL;
                 for (size_type i = 0; i < m_storage.size(); ++i)
                 {
                     if (item == m_storage[i])
-                        return &m_storage[i];
+                    {
+                        ret = &m_storage[i];
+                        break;
+                    }
                 }
-                return NULL;
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return ret;
             }
 
             const T* find(const T& item) const
             {
-                ReadGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                const T* ret = NULL;
                 for (size_type i = 0; i < m_storage.size(); ++i)
                 {
                     if (item == m_storage[i])
-                        return &m_storage[i];
+                    {
+                        ret = &m_storage[i];
+                        break;
+                    }
                 }
-                return NULL;
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return ret;
             }
 
             void clear()
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 m_storage.clear();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             T& operator[](size_type idx) 
             {
-                ReadGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
                 return m_storage[idx];
             }
 
             const T& operator[](size_type idx) const
             {
-                ReadGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_WAIT(RefLock);
                 return m_storage[idx];
             }
 
             T& at(size_type idx) 
             {
-                ReadGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_WAIT(RefLock);
                 return m_storage.at(idx);
             }
 
             T& front()
             {
-                ReadGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_WAIT(RefLock);
                 return m_storage.front();
             }
 
             T& back()
             {
-                ReadGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_WAIT(RefLock);
                 return m_storage.back();
             }
 
             const T& front() const
             {
-                ReadGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_WAIT(RefLock);
                 return m_storage.front();
             }
 
             const T& back() const
             {
-                ReadGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_WAIT(RefLock);
                 return m_storage.back();
             }
 
             iterator begin()
             {
-                ReadGuard Guard(GetLock());
-                return m_storage.begin();
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                iterator itr =  m_storage.begin();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return itr;
             }
 
             iterator end()
             {
-                ReadGuard Guard(GetLock());
-                return m_storage.end();
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                iterator itr =  m_storage.end();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return itr;
             }
 
             const_iterator begin() const
             {
-                ReadGuard Guard(GetLock());
-                return m_storage.begin();
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                const_iterator itr =  m_storage.begin();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return itr;
             }
 
             const_iterator end() const
             {
-                ReadGuard Guard(GetLock());
-                return m_storage.end();
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                const_iterator itr = m_storage.end();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return itr;
             }
 
             reverse_iterator rbegin()
             {
-                ReadGuard Guard(GetLock());
-                return m_storage.rbegin();
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                reverse_iterator itr = m_storage.rbegin();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return itr;
             }
 
             reverse_iterator rend()
             {
-                ReadGuard Guard(GetLock());
-                return m_storage.rend();
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                reverse_iterator itr = m_storage.rend();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return itr;
             }
 
             const_reverse_iterator rbegin() const
             {
-                ReadGuard Guard(GetLock());
-                return m_storage.rbegin();
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                const_reverse_iterator itr =  m_storage.rbegin();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return itr;
             }
 
             const_reverse_iterator rend() const
             {
-                ReadGuard Guard(GetLock());
-                return m_storage.rend();
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                const_reverse_iterator itr = m_storage.rend();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return itr;
             }
 
             bool empty() const
             {
-                ReadGuard Guard(GetLock());
-                return    m_storage.empty();
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                bool ret = m_storage.empty();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return ret;
             }
 
             size_type size() const
             {
-                ReadGuard Guard(GetLock());
-                return    m_storage.size();
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                size_type ret = m_storage.size();
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return ret;
             }
 
             LockedVector& operator=(const std::vector<T> &v)
             {
-                clear();
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                m_storage.clear();
                 for (typename std::vector<T>::const_iterator i = v.begin(); i != v.end(); ++i) 
                 {
                     this->push_back(*i);
                 }
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
                 return *this;
             }
 
             LockedVector(const std::vector<T> &v)
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 for (typename std::vector<T>::const_iterator i = v.begin(); i != v.end(); ++i) 
                 {
                     this->push_back(*i);
                 }
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             LockedVector& operator=(const std::list<T> &v)
             {
-                clear();
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                m_storage.clear();
                 for (typename std::list<T>::const_iterator i = v.begin(); i != v.end(); ++i) 
                 {
                     this->push_back(*i);
                 }
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
                 return *this;
             }
 
             LockedVector(const std::list<T> &v)
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 for (typename std::list<T>::const_iterator i = v.begin(); i != v.end(); ++i) 
                 {
                     this->push_back(*i);
                 }
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             LockedVector& operator=(const LockedVector<T> &v)
             {
-                WriteGuard Guard(GetLock());
-                ReadGuard GuardX(v.GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                MANGOSR2_ATOMIC_LOCK_BEGIN(v.RefLock);
                 m_storage = v.m_storage;
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                MANGOSR2_ATOMIC_LOCK_END(v.RefLock);
                 return *this;
             }
 
             LockedVector(const LockedVector<T> &v)
             {
-                WriteGuard Guard(GetLock());
-                ReadGuard GuardX(v.GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                MANGOSR2_ATOMIC_LOCK_BEGIN(v.RefLock);
                 m_storage = v.m_storage;
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                MANGOSR2_ATOMIC_LOCK_END(v.RefLock);
             }
 
             void swap(LockedVector<T, Allocator>& x)
             {
-                WriteGuard Guard(GetLock());
-                WriteGuard GuardX(x.GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                MANGOSR2_ATOMIC_LOCK_BEGIN(x.RefLock);
                 m_storage.swap(x.m_storage); 
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                MANGOSR2_ATOMIC_LOCK_END(x.RefLock);
             }
 
             void resize(size_type num, T def = T())
             {
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
                 m_storage.resize(num, def);
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
 
             //Allocator
             allocator_type get_allocator() const
             {
-                ReadGuard Guard(GetLock());
-                return m_storage.get_allocator(); 
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                allocator_type ret = m_storage.get_allocator(); 
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
+                return ret; 
             }
 
             // Sort template
             template <typename C>
             void sort(C& compare)
             {
-                iterator _begin = begin();
-                iterator _end   = end();
-                WriteGuard Guard(GetLock());
+                MANGOSR2_ATOMIC_LOCK_BEGIN(RefLock);
+                iterator _begin = m_storage.begin();
+                iterator _end   = m_storage.end();
                 std::stable_sort(_begin,_end,compare);
+                MANGOSR2_ATOMIC_LOCK_END(RefLock);
             }
-
-            LockType&       GetLock() { return i_lock; }
-            LockType&       GetLock() const { return i_lock; }
 
             // may be used _ONLY_ with external locking!
             std::vector<T>&  getSource()
@@ -347,7 +398,6 @@ namespace ACE_Based
             }
 
         protected:
-            mutable LockType           i_lock;
             std::vector<T, Allocator>  m_storage;
     };
 }
