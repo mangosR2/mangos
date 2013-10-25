0b7fc3b9ad8347850f252ee69944845298dc9fc7
fallenangelX <noneya@hotmail.com>
2013-10-25 04:15:27 -0400

    just some sql stuff
    Signed-off-by: fallenangelX <noneya@hotmail.com>

--- Infinity_R2DB_Installer/Development/mangosR2/mr02564_mangos_vehicle_accessory.sql
+++ Infinity_R2DB_Installer/Development/mangosR2/mr02564_mangos_vehicle_accessory.sql
@@ -1,3 +1,9 @@
+--
+-- `vehicle_accessory`
+--
+
+UPDATE `vehicle_accessory` SET `flags` = 1 WHERE `vehicle_entry` IN (27626, 28312, 32627, 32930, 33109, 33060, 33113, 33114, 36678, 33214, 33778, 33687, 28018, 28614, 35069, 34776);
+
 -- Hardcoded item Id's. Hide vehicle accessory.
 
 UPDATE vehicle_accessory SET flags = flags | 2 WHERE accessory_entry IN (33114, 33167);
--- sql_mr/mr02564_mangos_vehicle_accessory.sql
+++ sql_mr/mr02564_mangos_vehicle_accessory.sql
@@ -1,3 +1,8 @@
 -- Hardcoded item Id's. Hide vehicle accessory.
+--
+-- `vehicle_accessory`
+--
+
+UPDATE `vehicle_accessory` SET `flags` = 1 WHERE `vehicle_entry` IN (27626, 28312, 32627, 32930, 33109, 33060, 33113, 33114, 36678, 33214, 33778, 33687, 28018, 28614, 35069, 34776);
 
 UPDATE vehicle_accessory SET flags = flags | 2 WHERE accessory_entry IN (33114, 33167);
