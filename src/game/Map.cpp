bd98d5e20ce77cf73ec10792555ca0f7e56642f4
rsa <kris13@bk.ru>
2013-11-10 12:11:10 +0400

    move locks for WorldObjectEventProcessor class into class v0.3. must give more correct parallelize threads load. testing. - fix deadlock in TemporarySummon - remove one more unneeded lock - fix freeze in AIM_Initialize

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -2816,13 +2816,11 @@ WorldObjectEventProcessor* Map::GetEvents()
 
 void Map::KillAllEvents(bool force)
 {
-    WriteGuard Guard(GetLock(MAP_LOCK_TYPE_MAPOBJECTS), true);
     GetEvents()->KillAllEvents(force);
 }
 
 void Map::AddEvent(BasicEvent* Event, uint64 e_time, bool set_addtime)
 {
-    WriteGuard Guard(GetLock(MAP_LOCK_TYPE_MAPOBJECTS), true);
     if (set_addtime)
         GetEvents()->AddEvent(Event, GetEvents()->CalculateTime(e_time), set_addtime);
     else
@@ -2831,10 +2829,7 @@ void Map::AddEvent(BasicEvent* Event, uint64 e_time, bool set_addtime)
 
 void Map::UpdateEvents(uint32 update_diff)
 {
-    {
-        ReadGuard Guard(GetLock(MAP_LOCK_TYPE_MAPOBJECTS), true);
-        GetEvents()->RenewEvents();
-    }
+    GetEvents()->RenewEvents();
     GetEvents()->Update(update_diff);
 }
 
